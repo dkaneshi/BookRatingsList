8371f94f015e9921c3706387813dfdefb3d2ff72
dkaneshi <dkaneshi@gmail.com>
2025-06-15 13:49:14 -1000

    feat: update branding to BookList with new logo and revised navigation links

--- resources/views/components/layouts/booklist.blade.php
+++ resources/views/components/layouts/booklist.blade.php
@@ -11,10 +11,12 @@
 </head>
 <body class="min-h-screen bg-white dark:bg-zinc-800">
 <flux:header container class="bg-zinc-50 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-700">
-    <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+    <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left"/>
 
-    <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc." class="max-lg:hidden dark:hidden" />
-    <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc." class="max-lg:hidden! hidden dark:flex" />
+    <flux:brand href="/" logo="{{ asset('logoipsum-357.png') }}" name="BookList"
+                class="max-lg:hidden dark:hidden"/>
+    <flux:brand href="/" logo="{{ asset('logoipsum-357.png') }}" name="BookList"
+                class="max-lg:hidden! hidden dark:flex"/>
 
     <flux:navbar class="-mb-px max-lg:hidden">
         <flux:navbar.item icon="home" href="/" current>Home</flux:navbar.item>
@@ -23,35 +25,21 @@
         <flux:separator vertical variant="subtle" class="my-2"/>
     </flux:navbar>
 
-    <flux:spacer />
+    <flux:spacer/>
 
     <flux:navbar class="me-4">
-        <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
-        <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
-        <flux:navbar.item class="max-lg:hidden" icon="information-circle" href="#" label="Help" />
+        <flux:navbar.item class="max-lg:hidden" icon="arrow-right-end-on-rectangle" href="/login" label="Log in">Log in</flux:navbar.item>
+        <flux:navbar.item class="max-lg:hidden" icon="pencil-square" href="/register" label="Register">Register</flux:navbar.item>
     </flux:navbar>
 
-    <flux:dropdown position="top" align="start">
-        <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
-
-        <flux:menu>
-            <flux:menu.radio.group>
-                <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
-                <flux:menu.radio>Truly Delta</flux:menu.radio>
-            </flux:menu.radio.group>
-
-            <flux:menu.separator />
-
-            <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
-        </flux:menu>
-    </flux:dropdown>
 </flux:header>
 
-<flux:sidebar stashable sticky class="lg:hidden bg-zinc-50 dark:bg-zinc-900 border rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
-    <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+<flux:sidebar stashable sticky
+              class="lg:hidden bg-zinc-50 dark:bg-zinc-900 border rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
+    <flux:sidebar.toggle class="lg:hidden" icon="x-mark"/>
 
-    <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc." class="px-2 dark:hidden" />
-    <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc." class="px-2 hidden dark:flex" />
+    <flux:brand href="/" logo="{{ asset('logoipsum-357.png') }}" name="BookList" class="px-2 dark:hidden"/>
+    <flux:brand href="/" logo="{{ asset('logoipsum-357.png') }}" name="BookList" class="px-2 hidden dark:flex"/>
 
     <flux:navlist variant="outline">
         <flux:navlist.item icon="home" href="/books" current>Home</flux:navlist.item>
@@ -61,8 +49,8 @@
     <flux:spacer />
 
     <flux:navlist variant="outline">
-        <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
-        <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
+        <flux:navlist.item icon="arrow-right-end-on-rectangle" href="/login">Log in</flux:navlist.item>
+        <flux:navlist.item icon="pencil-square" href="/register">Register</flux:navlist.item>
     </flux:navlist>
 </flux:sidebar>
 
@@ -70,8 +58,6 @@
     {{ $slot }}
 </flux:main>
 
-@fluxScripts
-</body>
 @fluxScripts
 </body>
 </html>
