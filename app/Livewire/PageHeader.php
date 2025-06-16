c4250a02adb2ba4c7e296b7642129f806f72b9c8
dkaneshi <dkaneshi@gmail.com>
2025-06-15 14:23:42 -1000

    feat: personalize PageHeader name using authenticated user info

--- app/Livewire/PageHeader.php
+++ app/Livewire/PageHeader.php
@@ -2,15 +2,17 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class PageHeader extends Component
 {
-    public string $name = 'Guest';
+    public string $name;
     public string $subtitle;
 
     public function mount($subtitle): void
     {
+        $this->name = Auth::check() ? Auth::user()->name : 'Guest';
         $this->subtitle = $subtitle;
     }
 
