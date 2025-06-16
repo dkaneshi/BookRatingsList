c3b8981246fcb8a9387f8d2632b5083854979f44
dkaneshi <dkaneshi@gmail.com>
2025-06-16 08:29:59 -1000

    feat: show add a book link for authenticated user and show user dropdown menu.

--- resources/views/components/layouts/booklist.blade.php
+++ resources/views/components/layouts/booklist.blade.php
@@ -20,17 +20,68 @@
 
     <flux:navbar class="-mb-px max-lg:hidden">
         <flux:navbar.item icon="home" href="/" current>Home</flux:navbar.item>
+        @auth
         <flux:navbar.item icon="inbox" href="/books/create">Add a Book</flux:navbar.item>
+        @endauth
 
         <flux:separator vertical variant="subtle" class="my-2"/>
     </flux:navbar>
 
     <flux:spacer/>
 
-    <flux:navbar class="me-4">
-        <flux:navbar.item class="max-lg:hidden" icon="arrow-right-end-on-rectangle" href="/login" label="Log in">Log in</flux:navbar.item>
-        <flux:navbar.item class="max-lg:hidden" icon="pencil-square" href="/register" label="Register">Register</flux:navbar.item>
-    </flux:navbar>
+    @auth
+        <flux:dropdown position="top" align="end">
+            <flux:profile
+                class="cursor-pointer"
+                :initials="auth()->user()->initials()"
+            />
+
+            <flux:menu>
+                <flux:menu.radio.group>
+                    <div class="p-0 text-sm font-normal">
+                        <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                    <span
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
+                                    >
+                                        {{ auth()->user()->initials() }}
+                                    </span>
+                                </span>
+
+                            <div class="grid flex-1 text-start text-sm leading-tight">
+                                <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
+                                <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </flux:menu.radio.group>
+
+                <flux:menu.separator/>
+
+                <flux:menu.radio.group>
+                    <flux:menu.item :href="route('settings.profile')" icon="cog"
+                                    wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                </flux:menu.radio.group>
+
+                <flux:menu.separator/>
+
+                <form method="POST" action="{{ route('logout') }}" class="w-full">
+                    @csrf
+                    <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                        {{ __('Log Out') }}
+                    </flux:menu.item>
+                </form>
+            </flux:menu>
+        </flux:dropdown>
+    @else
+        <flux:navbar class="me-4">
+            <flux:navbar.item class="max-lg:hidden" icon="arrow-right-end-on-rectangle" href="/login" label="Log in">Log
+                in
+            </flux:navbar.item>
+            <flux:navbar.item class="max-lg:hidden" icon="pencil-square" href="/register" label="Register">Register
+            </flux:navbar.item>
+        </flux:navbar>
+    @endauth
 
 </flux:header>
 
@@ -46,12 +97,15 @@
         <flux:navlist.item icon="inbox" href="/books/create">Add a Book</flux:navlist.item>
     </flux:navlist>
 
-    <flux:spacer />
+    <flux:spacer/>
 
-    <flux:navlist variant="outline">
-        <flux:navlist.item icon="arrow-right-end-on-rectangle" href="/login">Log in</flux:navlist.item>
-        <flux:navlist.item icon="pencil-square" href="/register">Register</flux:navlist.item>
-    </flux:navlist>
+    @auth
+    @else
+        <flux:navlist variant="outline">
+            <flux:navlist.item icon="arrow-right-end-on-rectangle" href="/login">Log in</flux:navlist.item>
+            <flux:navlist.item icon="pencil-square" href="/register">Register</flux:navlist.item>
+        </flux:navlist>
+    @endauth
 </flux:sidebar>
 
 <flux:main container>
