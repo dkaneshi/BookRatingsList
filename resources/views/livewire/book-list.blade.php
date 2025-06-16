32a588137228fc9f26e6491ceccd09fafaa38730
dkaneshi <dkaneshi@gmail.com>
2025-06-16 09:15:56 -1000

    feat: show delete button on book cards only if user is authenticated

--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -11,9 +11,11 @@
                 <a href="#" aria-label="{{ $book->title }}">
                     <li wire:key="{{ $book->id }}">
                         <flux:card size="sm" class="hover:bg-zinc-50 dark:hover:bg-zinc-700">
-                            <flux:button class="float-end mt-2" wire:click="delete({{ $book->id }})">
-                                Delete
-                            </flux:button>
+                            @auth
+                                <flux:button class="float-end mt-2" wire:click="delete({{ $book->id }})">
+                                    Delete
+                                </flux:button>
+                            @endauth
                             <flux:heading class="flex items-center gap-2">{{ $book->title }}</flux:heading>
                             <flux:text class="mt-2">{{ $book->author }}</flux:text>
                             <flux:text>Rating: {{ $book->rating }}/10</flux:text>
