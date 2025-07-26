7c6cc92c72c75f98ff9508c09b69bda0ec77d69e
dkaneshi <dkaneshi@gmail.com>
2025-07-26 07:30:19 -1000

    fix: update rating validation rule from "int" to "integer" in BookForm

--- app/Livewire/Forms/BookForm.php
+++ app/Livewire/Forms/BookForm.php
@@ -15,6 +15,6 @@ final class BookForm extends Form
     #[Validate('required|string|min:3|max:255', onUpdate: false)]
     public string $author = '';
 
-    #[Validate('required|int|min:1|max:10', onUpdate: false)]
+    #[Validate('required|integer|min:1|max:10', onUpdate: false)]
     public int $rating = 1;
 }
