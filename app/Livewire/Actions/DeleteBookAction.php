7943125b1efd81bad3d1d5bebff6b234f1c0ead6
dkaneshi <dkaneshi@gmail.com>
2025-07-26 08:02:02 -1000

    refactor: enforce strict types and make DeleteBookAction final

--- app/Livewire/Actions/DeleteBookAction.php
+++ app/Livewire/Actions/DeleteBookAction.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Actions;
 
 use App\Models\Book;
 
-class DeleteBookAction
+final class DeleteBookAction
 {
     public function handle(Book $book): void
     {
