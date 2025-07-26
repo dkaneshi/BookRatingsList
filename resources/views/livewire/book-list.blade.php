f2324601257923d2ffa4d43d04b288786580e45d
dkaneshi <dkaneshi@gmail.com>
2025-07-26 07:39:28 -1000

    refactor: improve book list with separate edit and delete actions including confirmation dialog for deletion

--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -8,20 +8,30 @@
     <ul class="list">
         <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             @foreach ($books as $book)
-                <a href="/books/edit/{{ $book->id }}" aria-label="{{ $book->title }}">
-                    <li wire:key="{{ $book->id }}">
-                        <flux:card size="sm" class="hover:bg-zinc-50 dark:hover:bg-zinc-700">
-                            @auth
-                                <flux:button class="float-end mt-2" wire:click="delete({{ $book->id }})">
-                                    Delete
-                                </flux:button>
-                            @endauth
-                            <flux:heading class="flex items-center gap-2">{{ $book->title }}</flux:heading>
-                            <flux:text class="mt-2">{{ $book->author }}</flux:text>
-                            <flux:text>Rating: {{ $book->rating }}/10</flux:text>
-                        </flux:card>
-                    </li>
-                </a>
+                <li wire:key="{{ $book->id }}">
+                    <flux:card size="sm" class="hover:bg-zinc-50 dark:hover:bg-zinc-700">
+                        @auth
+                            <flux:button
+                                class="float-end mt-2"
+                                wire:click="delete({{ $book->id }})"
+                                aria-label="Delete book: {{ $book->title }}"
+                                wire:confirm.prompt="Are you sure you?\n\nType DELETE to confirm|DELETE"
+                                icon="trash"
+                                alt="Delete">
+                            </flux:button>
+                            <flux:button
+                                class="float-end mt-2"
+                                href="/books/edit/{{ $book->id }}"
+                                aria-label="Edit book: {{ $book->title }}"
+                                icon="pencil"
+                                alt="Edit">
+                            </flux:button>
+                        @endauth
+                        <flux:heading class="flex items-center gap-2">{{ $book->title }}</flux:heading>
+                        <flux:text class="mt-2">{{ $book->author }}</flux:text>
+                        <flux:text>Rating: {{ $book->rating }}/10</flux:text>
+                    </flux:card>
+                </li>
             @endforeach
         </div>
     </ul>
